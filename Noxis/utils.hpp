9c6debc2eb3f8c8150453b9e739f2e9533bd297c
Alexander Savchuk <mrxlinch@gmail.com>
2014-11-15 03:27:13 +0200

    Add comments to utils.hpp

--- Noxis/utils.hpp
+++ Noxis/utils.hpp
@@ -8,10 +8,28 @@ NOXIS_NS_BEGIN;
 
 class Node;
 
+/**
+ * @brief Convert degree to radians
+ * @param degree angle in degree
+ * @return angle in radians
+ */
 float toRadians(float degree);
 
+/**
+ * @brief Convert radians to degree
+ * @param radians angle in radians
+ * @return angle in degree
+ */
 float toDegrees(float radians); 
 
-void printTree(Node *startNode, std::ostream &os = std::cout, unsigned int level = 0);
+/**
+ * @brief Print tree of node as hierarchy list
+ * @param startNode start node
+ * @param os output stream
+ * @param levle start indent value
+ */
+void printTree(Node *startNode, 
+               std::ostream &os = std::cout, 
+               unsigned int level = 0);
 
 NOXIS_NS_END;
